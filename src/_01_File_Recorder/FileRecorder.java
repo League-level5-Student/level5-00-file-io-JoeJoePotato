0c52d370bbaac299c6c6452e8391b56ce80d8540
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2020-07-08 18:26:30 -0700

    BIOS

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,48 @@
 package _01_File_Recorder;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+public static void main(String[] args) {
+	String fp=JOptionPane.showInputDialog("What is your favorite pokemon?");
+	FileWriter fw;
+	try {
+		fw = new FileWriter("src/_01_File_Recorder/test.txt");
+
+	fw.write(fp);
+		
+	fw.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	try {
+		BufferedReader br = new BufferedReader(new FileReader("src/_01_File_Recorder/test.txt"));
+		
+		String line = br.readLine();
+		while(line != null){
+			System.out.println(line);
+			line = br.readLine();
+		}
+		
+		br.close();
+	} catch (FileNotFoundException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+	
+}
 }
